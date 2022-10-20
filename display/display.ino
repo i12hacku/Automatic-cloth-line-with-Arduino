de697f61d7b4d2d9a7ee7c4ffe4860a56853d0fa
Z HACKER <85310255+i12hacku@users.noreply.github.com>
2022-10-20 10:08:32 +0530

    Update display.ino

--- display/display.ino
+++ display/display.ino
@@ -6,12 +6,16 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
-  lcd.begin(16, 2);
 
-
+	lcd.begin();
+	lcd.backlight();
+	lcd.clear();
 	lcd.setCursor(0,0);
-	lcd.print("test");
+	lcd.print("Hello World");
+
+ 
 }
+
 
 void loop()
 {
